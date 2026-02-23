b5449650ce731a0a95943040b84cd5171e850077
itiguila-2024295 <itiguila-2024295@kinal.edu.gt>
2026-02-23 12:00:51 -0600

    feat: Implementación de capa Api

---
+++ src/AuthServiceChatter.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+using AuthServiceChatter.Application.Interfaces;
+using AuthServiceChatter.Application.Services;
+using AuthServiceChatter.Domain.Interfaces;
+using AuthServiceChatter.Persistence.Data;
+using AuthServiceChatter.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServiceChatter.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention());
+        
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserManagementService, UserManagementService>();
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+        services.AddScoped<ICloudinaryService, CloudinaryService>();
+        services.AddScoped<IEmailService, EmailService>();
+
+        services.AddHealthChecks();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
